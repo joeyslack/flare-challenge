0f8bf2f3423b6f3206f194d59a28414bf63390f0
Joseph Slack <josephdslack@gmail.com>
2015-12-19 20:45:49 -0800

    new input and key configs for 3rd party

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 module.exports = function() {
     return {
-        site: "http://flareapp.co/"
+        site: "http://flareapp.co/",
+        service_path: "https://boiling-gorge-3368.herokuapp.com/",
+        branch_api: "https://api.branch.io/v1/url",
+        branch_credit: "https://api.branch.io/v1/credits",
+        branch_key: "key_live_nboN9E5Mb42YrMiEQlRi9ebpyrdlyvCY",
+        branch_secret: "secret_live_cqbazwKj7qgWeTfJbCJTAmVbckACBDZL"
     };
 }
