fe54bf9c5691fe2b260f2aff6c81e2f20ebaba0a
Joseph Slack <josephdslack@gmail.com>
2015-12-19 23:46:22 -0800

    Add twilio keys

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ module.exports = function() {
         branch_api: "https://api.branch.io/v1/url",
         branch_credit: "https://api.branch.io/v1/credits",
         branch_key: "key_live_nboN9E5Mb42YrMiEQlRi9ebpyrdlyvCY",
-        branch_secret: "secret_live_cqbazwKj7qgWeTfJbCJTAmVbckACBDZL"
+        branch_secret: "secret_live_cqbazwKj7qgWeTfJbCJTAmVbckACBDZL",
+        twilio_sid: "ACe5c37bde8428d07087bce86f7c3a7de7",
+        twilio_token = "cdfc481704a7ae869a3cfc499c0f36bc"
     };
 }
